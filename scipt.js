d195d648af14d0fcfdd6ed3cf1e8922f2a3cb651
Leonardo Campos <Leonardocamposdev777@gmail.com>
2024-04-15 22:15:32 -0300

    update

---
+++ scipt.js
@@ -0,0 +1,14 @@
+//MENU HAMBURGER//
+const btnMobile = document.querySelector(".btn-mobile");
+const headerMenu = document.querySelector(".header-menu");
+
+function ToggleMenu(){
+  const nav = document.querySelector(".nav");
+  const headerMenu = document.querySelector(".header-menu");
+  nav.classList.toggle("active");
+  headerMenu.classList.toggle("active");
+}
+
+
+btnMobile.addEventListener("click",ToggleMenu);
+headerMenu.addEventListener("click", ToggleMenu);
